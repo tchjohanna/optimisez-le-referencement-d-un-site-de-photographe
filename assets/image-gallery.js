19f99bc850d5bc6dc3be6db572ba8866f6b51d95
Votre Nom <tchjohanna@icloud.com>
2023-11-19 16:11:21 +0100

    updates carrousel version mobile

--- assets/image-gallery.js
+++ assets/image-gallery.js
@@ -2,77 +2,199 @@
 	const images = [
 	  {
 		tag: "concerts",
-		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash", 
+		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-636.webp", 
+	  },
+	  {
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-extrasmall.webp", // Chemin de l'image 2000x2000
+	  },
+	  {
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-small.webp", // Chemin de l'image 636x636
+	  },
+	  {
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-medium.webp", // Chemin de l'image 2000x2000
+	  },
+	  {
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-large.webp", // Chemin de l'image 636x636
 	  },
 	  {
     	tag: "concerts",
-    	path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash",
+    	path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-636",
   	  },
+	{
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-extrasmall",
+	},
+	{
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-small",
+	},
+	{
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-medium",
+	},
+	{
+		tag: "concerts",
+		path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-large",
+	},
+ 	{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-636",
+	},
+	{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-extrasmall",
+	},
+	{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-small",
+	},
+	{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-medium",
+	},
+	{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-large",
+	},
 	  {
 		tag: "entreprise",
-		path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash",
+		path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-636",
 	  },
 	  {
 		tag: "entreprise",
-		path: "assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash",
+		path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-extrasmall",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-small",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-medium",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-large",
 	  },
 	  {
 		tag: "entreprise",
-		path: "assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash",
+		path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-636",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-extrasmall",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-small",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-medium",
+	  },{
+		tag: "entreprise",
+		path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-large",
 	  },
 	  {
 		tag: "mariage",
-		path: "assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash",
+		path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-636",
 	  },
 	  {
 		tag: "mariage",
-		path: "assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash",
+		path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-extrasmall",
+	  },{
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-small",
+	  },{
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-medium",
+	  },
+	  {
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-636",
+	  },
+	  {
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-small",
+	  },{
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-medium",
+	  },{
+		tag: "mariage",
+		path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-large",
 	  },
 	  {
 		tag: "portraits",
-		path: "assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash",
+		path: ".assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-636",
 	  },
 	  {
 		tag: "portraits",
-		path: "assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash",
+		path: ".assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-extrasmall",
+	  },{
+		tag: "portraits",
+		path: ".assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-small",
+	  },{
+		tag: "portraits",
+		path: ".assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-medium",
+	  },{
+		tag: "portraits",
+		path: ".assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-large",
+	  },
+	  {
+		tag: "portraits",
+		path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-636",
+	  },
+	  {
+		tag: "portraits",
+		path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-extrasmall",
+	  },
+	  {
+		tag: "portraits",
+		path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-small",
+	  },
+	  {
+		tag: "portraits",
+		path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-medium",
+	  },
+	  {
+		tag: "portraits",
+		path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-large",
 	  },
 	];
 	    
 	function generateImageTags() {
-        const container = document.querySelector('.gallery');
-        container.innerHTML = '';
-
-        images.forEach((image) => {
-            const img = document.createElement('img');
-            img.classList.add('gallery-item', 'img-fluid');
-            img.setAttribute('data-gallery-tag', image.tag);
-            img.setAttribute('alt', `Image représentant ${image.tag}`);
-            
-            // Utilisez la nouvelle image 636x636 comme source par défaut
-            img.src = `${image.path}-636.webp`; 
-            
-            // Incluez la nouvelle taille d'image dans le srcset
-            img.srcset = `
-              ${image.path}-extrasmall.webp 300w,
-              ${image.path}-small.webp 600w,
-              ${image.path}-636.webp 636w, // Nouvelle taille ajoutée
-              ${image.path}-medium.webp 1200w,
-              ${image.path}-large.webp 2000w,
-              
-            `;
-
-            // Assurez-vous que l'attribut sizes correspond aux points d'arrêt réels de votre design
-            img.sizes = `
-  				(max-width: 300px) 300px,
-  				(max-width: 600px) 600px,
-  				(max-width: 636px) 636px,
-  				(max-width: 900px) 900px,
-  				(max-width: 1200px) 1200px,
-  				
+		const container = document.querySelector('.gallery');
+		container.innerHTML = '';
+	
+		images.forEach((image) => {
+			const img = document.createElement('img');
+			img.classList.add('gallery-item', 'img-fluid');
+			img.setAttribute('data-gallery-tag', image.tag);
+			img.setAttribute('alt', `Image représentant ${image.tag}`);
+	
+			// Utilisez la nouvelle image 636x636 comme source par défaut
+			const imagePath = `${image.path}-636.webp`; 
+			img.src = imagePath;
+	
+			// Incluez la nouvelle taille d'image dans le srcset
+			img.srcset = `
+			  ${imagePath} 636w, // Nouvelle taille ajoutée
+			  ${image.path}-extrasmall.webp 300w,
+			  ${image.path}-small.webp 600w,
+			  ${image.path}-medium.webp 1200w,
+			  ${image.path}-large.webp 2000w,
 			`;
-
-            container.appendChild(img);
-        });
-    }
-
-    document.addEventListener('DOMContentLoaded', generateImageTags);
+	
+			// Assurez-vous que l'attribut sizes correspond aux points d'arrêt réels de votre design
+			img.sizes = `
+			  (max-width: 300px) 300px,
+			  (max-width: 600px) 600px,
+			  (max-width: 636px) 636px,
+			  (max-width: 900px) 900px,
+			  (max-width: 1200px) 1200px,
+			`;
+	
+			container.appendChild(img);
+		});
+	}
+	
+	document.addEventListener('DOMContentLoaded', generateImageTags);
+	
