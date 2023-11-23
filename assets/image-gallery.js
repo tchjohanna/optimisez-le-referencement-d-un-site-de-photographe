ca92e25e9083cae672882f689657bdf73ac6267a
Votre Nom <tchjohanna@icloud.com>
2023-11-23 16:30:04 +0100

    maj photos

--- assets/image-gallery.js
+++ assets/image-gallery.js
@@ -1,46 +1,46 @@
 const images = [
 	{
 	  tag: "concerts",
-	  path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-webp", 
+	  path: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.webp", 
 	},
 	
 	{
 	  tag: "concerts",
-	  path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-webp",
+	  path: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.webp",
 	  },
   
    {
 	  tag: "entreprise",
-	  path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-webp",
+	  path: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.webp",
   },
   
 	{
 	  tag: "entreprise",
-	  path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-webp",
+	  path: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.webp",
 	},
 	
 	{
 	  tag: "entreprise",
-	  path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-webp",
+	  path: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.webp",
 	},
 	{
 	  tag: "mariage",
-	  path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-webp",
+	  path: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.webp",
 	},
 	
 	{
 	  tag: "mariage",
-	  path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-webp",
+	  path: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.webp",
 	},
 	
 	{
 	  tag: "portraits",
-	  path: "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-webp",
+	  path: "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.webp",
 	},
 	
 	{
 	  tag: "portraits",
-	  path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-webp",
+	  path: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.webp",
 	},
 	
 	
